20339e5d403301bb8f94f5563d924c7b1104423a
ryoung29 <robert.e.young@gmail.com>
2015-10-14 16:44:04 -0700

    removed git add config.fish function

--- config.fish
+++ config.fish
@@ -34,17 +34,4 @@ function killSL
     vboxmanage controlvm StarLiMS acpipowerbutton;
 end
 
-function !!
-  set var (history | head -n 1);
-  if test $argv;
-	  if test $argv = "sudo"        #; or "any other command you want to prepend"
-					  eval $argv $var
-	  else
-		  eval $var $argv
-	  end
-  else
-      eval $var
-  end
-end
-
 set fish_color_redirection white --bold
