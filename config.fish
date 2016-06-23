d5f4db2d1010109846792f8f0f5133f6e813220b
ryoung29 <robert.e.young@gmail.com>
2016-06-23 14:50:44 -0700

    added virtualenvwrapper functionality

--- config.fish
+++ config.fish
@@ -1,18 +1,21 @@
 date +"%B %d %Y %r"
 echo "Greetings, Robert!"
 
-alias dd=sudo dcfldd
+# aliases
+alias dd="sudo dcfldd"
 alias du=ncdu
 alias exot=exit
 alias free="free -m"
+set fish_color_redirection white --bold
+export set WORKON_HOME=Envs
+fish_vi_mode
+fish_vi_key_bindings
+fish_vi_cursor
 
+# shortcut functions
 
 function cdl
     cd $argv; and ls
-end
-
-function newstuff
-    sudo apt-get update; and sudo apt-get dist-upgrade;
 end
 
 function mkcd
@@ -20,12 +23,32 @@ function mkcd
     cd $argv;
 end
 
-set fish_color_redirection white --bold
+function newstuff
+    sudo apt-get update; and sudo apt-get dist-upgrade;
+end
 
 function conservebat
     sudo powertop --auto-tune; and sudo tlp start
 end
 
-fish_vi_mode
-fish_vi_key_bindings
-fish_vi_cursor
+# virtualenvwrapper-like functionality
+
+alias lsenvs="ls $WORKON_HOME"
+
+function -d "Like virtualenvwrapper for python2" mkvirtualenv2
+    virtualenv -p python2 $WORKON_HOME/$argv;
+    and source $WORKON_HOME/$argv/bin/activate.fish
+end
+
+function -d "Like virtualenvwrapper" mkvirtualenv
+    virtualenv -p python3 $WORKON_HOME/$argv;
+    and source $WORKON_HOME/$argv/bin/activate.fish
+end
+
+function workon
+    source $WORKON_HOME/$argv/bin/activate.fish
+end
+
+function -d "Like virtualenvwrapper" rmvirtualenv
+    deactivate; and rm -rf $WORKON_HOME/$argv^/dev/null; or rm -rf $WORKON_HOME/$argv
+end
